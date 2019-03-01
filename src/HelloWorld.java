824a2e1943d2e55612fdeb817b647a0b728d5b64
Tamir Shmueli <44590894+Tamirs2702@users.noreply.github.com>
2019-03-01 17:31:54 +0200

    Add files via upload

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+// import statements
+
+/**
+ * @author      tamir shmueli tamirs2702@gmail.com
+ * @version     1.6                 (current version number of program)
+ * @since       1.2          (the version of the package this class was first added to)
+ */
+
+public class HelloWorld {
+     /**
+    * This is a traditional demo class to demonstrate how to
+    *  write, build and run a Java program.
+    *
+    * @param args command line arguments.
+    */
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) { //for loop
+            System.out.println("Hi");
+        }
+    }
+}
+
